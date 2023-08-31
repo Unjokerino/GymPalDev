dba00e736452aff35ef5841d443f1f4572d44499
Unjokerino <35525106+Unjokerino@users.noreply.github.com>
2023-08-31 21:19:35 +0400

    feat: add navigation to the app

---
+++ src/navigation/types.ts
@@ -0,0 +1,9 @@
+export enum rootScreens {
+  BottomTabs = "BottomTabs",
+  EmptyPage = "EmptyPage",
+  Home = "Home",
+}
+
+export type RootStackParamList = {
+  [rootScreens.BottomTabs]: undefined;
+};
